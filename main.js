43e768b97c332262dd1b20116a57f9181e488646
Fan4ik1308 <moonaraves@gmail.com>
2025-03-01 10:53:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let rang = prompt("Rang kiriting");
+let textcolor = prompt("textni colorini kiriting");
+
+let newrang = document.createElement("p");
+document.body.style.backgroundColor = rang;
+document.body.append(newrang)
+
+let newtextcolor = document.createElement("p")
+document.body.style.color = textcolor;
+document.body.append(newtextcolor);
+
+
+
